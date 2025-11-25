48f77218ebcf92d1a05bb7348bf3dda863ba48b6
Kr1p0chek <145468395+Kr1p0chek@users.noreply.github.com>
2025-11-25 12:40:34 +0500

    Add files via upload

---
+++ task2-6.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+link = "http://suninjuly.github.io/alert_accept.html"
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    alert = browser.switch_to.alert
+    alert.accept()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = int(x_element.text)
+
+    result = math.log(abs(12 * math.sin(x)))
+
+    answer_field = browser.find_element(By.ID, "answer")
+    answer_field.send_keys(str(result))
+    
+    submit_button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    submit_button.click()
+
+    time.sleep(2)
+    alert = browser.switch_to.alert
+    answer_text = alert.text
+    print("Полученный ответ:", answer_text)
+    
+finally:
+    time.sleep(8)
+    browser.quit()
